9d8434dcd23253afd59ac13abe5ee744232fbb61
abel-tefera <40035531+abel-tefera@users.noreply.github.com>
2019-01-13 12:37:56 +0300

    Commits

---
+++ java/schoolsystem/domain/ClassRoom.java
@@ -0,0 +1,18 @@
+package schoolsystem.domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+
+public class ClassRoom {
+	private Long id;
+	private final GradeLevel gradeLevel;
+	private final Section section;	
+	private final String identity;
+
+	public String setIdentity() {
+		return this.gradeLevel.toString() + this.section.toString(); 
+	}
+}
---
+++ java/schoolsystems/repository/StudentRepository.java
@@ -0,0 +1,18 @@
+package schoolsystems.repository;
+
+import schoolsystem.domain.*;
+
+public interface StudentRepository {
+	Iterable<Student> findAll();
+
+	Iterable<Student> findByClass(ClassRoom classRoom);
+
+	Student findById(String id);
+
+	Student save(Student student);
+	
+	Iterable<Student> findByCourse(Subject subject);
+	
+	Iterable<Student> findByTeacher(Teacher teacher);
+
+}
